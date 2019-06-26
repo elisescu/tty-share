71ebda8e8f29553765856f5c253e41271793ba98
Vasile Popescu <elisescu@elisescu.com>
2019-06-26 22:59:18 +0200

    Refactor the whole receiver part
    - remove unused dependencies
    - move to typescript
    - add an automatic way to resize the tty-receiver

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,7 @@ const develBuild = process.env.TTY_SHARE_ENV === 'development';
 
 let mainConfig  = {
     entry: {
-        'tty-receiver': './tty-receiver/main.js',
+        'tty-receiver': './tty-receiver/main.ts',
     },
     output: {
         path: __dirname + '/public/',
@@ -14,44 +14,22 @@ let mainConfig  = {
     module: {
         rules: [
             {
-                test:/\.(js|jsx)$/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                      babelrc: false,
-                      presets: ['env', 'react'],
-                    },
-                  }],
-            },
-            {
-                test: /\.(tsx|ts)?$/,
-                use: ['awesome-typescript-loader']
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
             },
             {
                 test: /node_modules.+xterm.+\.map$/,
                 use: ['ignore-loader']
             },
             {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)/,
-                use: ['url-loader']
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: ['url-loader', 'image-webpack-loader']
-            },
-            {
-                test: /\.js\.map$/,
-                use: ['source-map-loader']
             }
         ]
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
     },
     plugins: [
         new copyWebpackPlugin([
