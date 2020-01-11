786f47b1057bba4da7b486158bf9e45144182367
Vasile Popescu <elisescu@elisescu.com>
2020-01-11 18:14:21 +0100

    Switch from `dep` to `go mod` deps management

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/elisescu/tty-share
+
+go 1.13
+
+require (
+	github.com/elisescu/pty v1.0.2
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/crypto v0.0.0-20191227163750-53104e6ec876
+	golang.org/x/sys v0.0.0-20200103143344-a1369afcdac7 // indirect
+)
--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,9 @@ VERSION=$(git describe --tags `git rev-list --tags --max-count=1` | awk '{print 
 OUTDIR=out
 
 mkdir -p ${OUTDIR} && \
-    GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-X main.version=${VERSION}" -o ${OUTDIR}/tty-share.rpi && \
-    GOOS=linux go build -ldflags "-X main.version=${VERSION}" -o ${OUTDIR}/tty-share.lin && \
-    GOOS=darwin go build -ldflags "-X main.version=${VERSION}" -o ${OUTDIR}/tty-share.mac && \
+    GOOS=linux GOARCH=arm GOARM=6 go build -mod=vendor -ldflags "-X main.version=${VERSION}" -o ${OUTDIR}/tty-share.rpi && \
+    GOOS=linux go build  -mod=vendor -ldflags "-X main.version=${VERSION}" -o ${OUTDIR}/tty-share.lin && \
+    GOOS=darwin go build  -mod=vendor -ldflags "-X main.version=${VERSION}" -o ${OUTDIR}/tty-share.mac && \
     zip ${OUTDIR}/tty-share.rpi.zip ${OUTDIR}/tty-share.rpi && \
     zip ${OUTDIR}/tty-share.lin.zip ${OUTDIR}/tty-share.lin && \
     zip ${OUTDIR}/tty-share.mac.zip ${OUTDIR}/tty-share.mac
