8b13be4d87966547d5c1f91d2b56e12944818a7f
Vasile Popescu <elisescu@elisescu.com>
2022-11-18 14:00:51 +0100

    Update the node modules

--- server/frontend/tty-share/main.ts
+++ server/frontend/tty-share/main.ts
@@ -2,7 +2,6 @@ import 'xterm/css/xterm.css';
 import './main.css';
 
 import { Terminal } from 'xterm';
-import * as pbkdf2 from 'pbkdf2';
 
 import { TTYReceiver } from './tty-receiver';
 
@@ -10,9 +9,6 @@ const term = new Terminal({
     cursorBlink: true,
     macOptionIsMeta: true,
 });
-
-const derivedKey = pbkdf2.pbkdf2Sync('password', 'salt', 4096, 32, 'sha256');
-console.log(derivedKey);
 
 let wsAddress = "";
 if (window.location.protocol === "https:") {
